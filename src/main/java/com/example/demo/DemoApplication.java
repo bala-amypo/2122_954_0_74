d0d849685b304750a3741c8e59e31de034b0534f
bala-amypo <bala@amypo.in>
2025-12-17 06:28:20 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,35 @@
-package com.example.demo;
+package com.example.demo.service;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.*;
+import org.springframework.stereotype.Service;
+import com.example.demo.entity.Student;
 
-@SpringBootApplication
-public class DemoApplication {
+@Service   
+public class StudentServiceImpl implements StudentService {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    private final Map<Long, Student> store = new HashMap<>();
+    private long counter = 1;
 
+    @Override
+    public Student insertStudent(Student st) {
+        st.setId(counter++);
+        store.put(st.getId(), st);
+        return st;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return new ArrayList<>(store.values());
+    }
+
+    @Override
+    public Optional<Student> getOneStudent(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public void deleteStudent(Long id) {
+        store.remove(id);
+    }
 }
+ 
