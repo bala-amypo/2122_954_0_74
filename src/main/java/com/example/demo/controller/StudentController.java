3904e30720e801914a0b4fb2021b8404319da028
bala-amypo <bala@amypo.in>
2025-12-17 04:20:41 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,57 @@
+package com.example.shagyeeen.controller;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.shagyeeen.entity.Student;
+import com.example.shagyeeen.service.StudentService;
+
+@RestControllerpublic class StudentController {
+
+    @AutowiredStudentService studentService;
+
+    @PostMapping("/PostStudent")
+    public Student postStd(@RequestBody Student st ) {
+        return studentService.insertStudent(st);
+    }
+    @GetMapping("/getAll")
+    public List<Student> gatAll(){
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/get/{id}")
+    public Optional<Student> get(@PathVariable Long id){
+        return studentService.getOneStudent(id);
+    }
+    @PutMapping("/update{id}")
+    public String updateStudent(@PathVariable Long id,@RequestBody Student st){
+        Optional<Student> student=studentService.getOneStudent(id);
+        if(student.isPresent()){
+            Student newStudent = student.get();
+            newStudent.setId(id);
+            studentService.insertStudent(newStudent);
+            return "Updated Success";
+        }
+        return "Student Not Found";
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteStudent(@PathVariable Long id){
+        Optional<Student> student=studentService.getOneStudent(id);
+        if(student.isPresent()){
+            studentService.deleteStudent(id);
+            return "Deleted Success";
+        }
+        return "Student Not Found";
+    }
+}
